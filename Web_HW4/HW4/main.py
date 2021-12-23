06b0acb99ac460e027d34fb7d7f118067f8e0a8c
Anton <nazarukav96@gmail.com>
2021-12-23 22:04:00 +0200

    "Fixed Web_HW 4 - separated on 2 modules, changed name of variable, add additional comments"

--- Web_HW4/HW4/main.py
+++ Web_HW4/HW4/main.py
@@ -1,59 +1,14 @@
 from concurrent.futures import ThreadPoolExecutor
 import os
 from time import time
+from settings import MAIN_PATH, EXTENSIONS, EXT_LIST
 
-#This is path, where program will be executed
-main_path = 'b:\\random folder' 
-
-#This is extensions dictionary. 
-#Keys are names of folders and values - extension of files in these folders
-extensions = { 
-
-    'video': ['mp4', 'mov', 'avi', 'mkv', 'wmv', '3gp', '3g2', 'mpg', 'mpeg', 'm4v', 'h264', 'flv',
-              'rm', 'swf', 'vob'],
-
-    'data': ['sql', 'sqlite', 'sqlite3', 'csv', 'dat', 'db', 'log', 'mdb', 'sav', 'tar', 'xml'],
-
-    'audio': ['mp3', 'wav', 'ogg', 'flac', 'aif', 'mid', 'midi', 'mpa', 'wma', 'wpl', 'cda'],
-
-    'image': ['jpg', 'png', 'bmp', 'ai', 'psd', 'ico', 'jpeg', 'ps', 'svg', 'tif', 'tiff'],
-
-    'archive': ['zip', 'rar', '7z', 'z', 'gz', 'rpm', 'arj', 'pkg', 'deb'],
-
-    'text': ['pdf', 'txt', 'doc', 'docx', 'rtf', 'tex', 'wpd', 'odt'],
-
-    '3d': ['stl', 'obj', 'fbx', 'dae', '3ds', 'iges', 'step'],
-
-    'presentation': ['pptx', 'ppt', 'pps', 'key', 'odp'],
-
-    'spreadsheet': ['xlsx', 'xls', 'xlsm', 'ods'],
-
-    'font': ['otf', 'ttf', 'fon', 'fnt'],
-
-    'gif': ['gif'],
-
-    'exe': ['exe'],
-
-    'bat': ['bat'],
-
-    'apk': ['apk']
-}
-# This is conversion of extension dict into list which looks like: [(key1,[val1,val2,val3]),(key2,[val3,val4,val5])]
-# Will be used later in sort_file_thread func
-ext_list = list(extensions.items())
-
-# Function to create folders from "extensions" dictionary
+# Function to create folders from "EXTENSIONS" dictionary
 # Check on existance of such folder included
 def create_folders_from_list(folder_path, folder_names):
     for folder in folder_names:
         if not os.path.exists(f'{folder_path}\\{folder}'):
             os.mkdir(f'{folder_path}\\{folder}')
-
-# Function to get subfolders path (of 1st level nesting)
-def get_subfolder_paths(folder_path) -> list:
-    subfolder_paths = [f.path for f in os.scandir(folder_path) if f.is_dir()] 
-
-    return subfolder_paths
 
 
 # Function to recursively check directory for files, diving inside every folder
@@ -84,18 +39,28 @@ def sort_file_thread(file_path):
     extension = file_path.split('.')[-1] # split extension from file path
     file_name = file_path.split('\\')[-1] # split filename from file path
 
-    for dict_key_int in range(len(ext_list)): # iterate throuth extension dict
-        if extension in ext_list[dict_key_int][1]: # check if extension is present in extension dict
-            print(f'Moving {file_name} in {ext_list[dict_key_int][0]} folder\n') 
-            os.rename(file_path, f'{main_path}\\{ext_list[dict_key_int][0]}\\{file_name}') # by renaming file path, we actualy move it to new folder
+    for ext_list_num in range(len(EXT_LIST)): # iterate throuth extension list
+        if extension in EXT_LIST[ext_list_num][1]: # check if extension is present in extension list
+            print(f'Moving {file_name} in {EXT_LIST[ext_list_num][0]} folder\n') 
+            os.rename(file_path, f'{MAIN_PATH}\\{EXT_LIST[ext_list_num][0]}\\{file_name}') # by renaming file path, we actualy move it to new folder
+# example of ext_list_num:
+# ('data', ['sql', 'sqlite', 'sqlite3', 'csv', 'dat', 'db', 'log', 'mdb', 'sav', 'tar', 'xml'),
+# ext_list_num[0] is filetype ( audio, text, 3d etc.)
+# ext_list_num[1] is extension (txt, sql, bin, png etc)
+
 
 # Func to run dedicated sort_file_thread func inside threads
 def run_file_sorting_threads():
-    file_paths = recurs_get_file_paths(main_path)
+    file_paths = recurs_get_file_paths(MAIN_PATH)
     # Running treads with sort_file_thread func
     with ThreadPoolExecutor(2) as pool:
         pool.map(sort_file_thread,file_paths)
 
+
+# Function to get subfolders path (of 1st level nesting)
+def get_subfolder_paths(folder_path) -> list:
+    subfolder_paths = [f.path for f in os.scandir(folder_path) if f.is_dir()] 
+    return subfolder_paths
 
 # Func to remove empty folders
 def remove_empty_folders(folder_path):
@@ -109,9 +74,9 @@ def remove_empty_folders(folder_path):
 
 if __name__ == "__main__":
     start_time = time()
-    create_folders_from_list(main_path, extensions)
+    create_folders_from_list(MAIN_PATH, EXTENSIONS)
     run_file_sorting_threads()
-    remove_empty_folders(main_path)
+    remove_empty_folders(MAIN_PATH)
     finish_time = time()-start_time
     print(finish_time)
 
---
+++ Web_HW4/HW4/settings.py
@@ -0,0 +1,41 @@
+#This is path, where program will be executed
+MAIN_PATH = 'B:\\Sorting folder' 
+
+#This is extensions dictionary. 
+#Keys are names of folders and values - extension of files in these folders
+EXTENSIONS = { 
+
+    'video': ['mp4', 'mov', 'avi', 'mkv', 'wmv', '3gp', '3g2', 'mpg', 'mpeg', 'm4v', 'h264', 'flv',
+              'rm', 'swf', 'vob'],
+
+    'data': ['sql', 'sqlite', 'sqlite3', 'csv', 'dat', 'db', 'log', 'mdb', 'sav', 'tar', 'xml'],
+
+    'audio': ['mp3', 'wav', 'ogg', 'flac', 'aif', 'mid', 'midi', 'mpa', 'wma', 'wpl', 'cda'],
+
+    'image': ['jpg', 'png', 'bmp', 'ai', 'psd', 'ico', 'jpeg', 'ps', 'svg', 'tif', 'tiff'],
+
+    'archive': ['zip', 'rar', '7z', 'z', 'gz', 'rpm', 'arj', 'pkg', 'deb'],
+
+    'text': ['pdf', 'txt', 'doc', 'docx', 'rtf', 'tex', 'wpd', 'odt'],
+
+    '3d': ['stl', 'obj', 'fbx', 'dae', '3ds', 'iges', 'step'],
+
+    'presentation': ['pptx', 'ppt', 'pps', 'key', 'odp'],
+
+    'spreadsheet': ['xlsx', 'xls', 'xlsm', 'ods'],
+
+    'font': ['otf', 'ttf', 'fon', 'fnt'],
+
+    'gif': ['gif'],
+
+    'exe': ['exe'],
+
+    'bat': ['bat'],
+
+    'apk': ['apk']
+}
+# This is conversion of extension dict into list which looks like: [(key1,[val1,val2,val3]),(key2,[val3,val4,val5])]
+# Will be used later in sort_file_thread func
+EXT_LIST = list(EXTENSIONS.items())
+
+# print(EXT_LIST)
